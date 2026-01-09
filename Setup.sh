ad68dc5fb7b04abc73beb20547055245ac68ecda
lucifer82051 <yujsjsyunu@gmail.com>
2026-01-09 15:48:10 +0300

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -2,4 +2,5 @@ pkg update -y
 pkg upgrade -y
 pkg install python3 -y
 pip install phonenumbers colorama
-python3 telefon_numarası_sorgusu.py
+cd Phone_number/phone_number_query.py
+python3 phone_number_query.py
